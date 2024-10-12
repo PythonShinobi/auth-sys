a3e5567655aa1ea7456752e0a5c6f26ae78a4b4c
PythonShinobi <pythonshinobi@gmail.com>
2024-10-12 14:15:07 +0300

    Generate new migration script for new database sqlite.

--- server/app/config.py
+++ server/app/config.py
@@ -4,18 +4,13 @@ from dotenv import load_dotenv
 # Load environment variable from .env file
 load_dotenv()
 
+db_path = os.path.join(os.path.dirname(__file__), 'app.db')
+
 class Config:
     # Default configuration
-    SECRET_KEY="t8hrNO6ibFODgZWxhunyhQ"
-    FRONTEND_ENDPOINT = 'http://localhost:3000'
+    SECRET_KEY = "t8hrNO6ibFODgZWxhunyhQ"
+    FRONTEND_ENDPOINT = 'http://localhost:3000'    
 
-    # Database configuration
-    DB_HOST = os.getenv('DB_HOST')
-    USER = os.getenv('USER')
-    PASSWORD = os.getenv('PASSWORD')
-    DATABASE = os.getenv('DATABASE')
-    DB_PORT = os.getenv('DB_PORT')
-
-    # Set the SQLAlchemy database URI
-    SQLALCHEMY_DATABASE_URI = f'postgresql://{USER}:{PASSWORD}@{DB_HOST}:{DB_PORT}/{DATABASE}'
+    # Set the SQLAlchemy database URI for SQLite
+    SQLALCHEMY_DATABASE_URI = f'sqlite:///{db_path}'  # SQLite URI format
     SQLALCHEMY_TRACK_MODIFICATIONS = False
